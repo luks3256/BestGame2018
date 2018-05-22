42401a2a3607ddb085c736b76d8c14fa88353df9
luks3256 <37143960+luks3256@users.noreply.github.com>
2018-05-22 21:27:36 +0300

    Manu pilnai
    Viskas

---
+++ PC TEST/Assets/SCRIPTS/ObjectDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroy : MonoBehaviour {
+
+    public GameObject ObjectToDestroy;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag== "Player")
+        {
+            Debug.Log("hit");
+            Destroy(gameObject);
+        }
+    }
+}
